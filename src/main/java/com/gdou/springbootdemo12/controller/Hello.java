a2bac0c2553e055fbcff608a898c339d3ee8fd11
booleanxing <duanxing2007@gmail.com>
2019-09-06 09:28:17 +0800

    hello

---
+++ src/main/java/com/gdou/springbootdemo12/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.gdou.springbootdemo12.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
