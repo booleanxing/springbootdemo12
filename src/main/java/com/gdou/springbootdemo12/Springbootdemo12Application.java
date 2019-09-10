375b642e0b1f88c72848a3a23746aedc0598eb1f
booleanxing <duanxing2007@gmail.com>
2019-09-10 10:30:21 +0800

    启动器

---
+++ src/main/java/com/gdou/springbootdemo12/Springbootdemo12Application.java
@@ -0,0 +1,13 @@
+package com.gdou.springbootdemo12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo12Application.class, args);
+    }
+
+}
